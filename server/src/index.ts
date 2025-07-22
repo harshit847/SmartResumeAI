6efa37ce08fe4209ac034f2e2a706ad66b300c00
harshit847 <harshitrai331@gmail.com>
2025-07-22 23:52:08 +0530

    New thing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoutes from "./src/routes/auth.routes";
-import resumeRoutes from "./src/routes/resume.routes";
+import authRoutes from "./routes/auth.routes";
+import resumeRoutes from "./routes/resume.routes";
 
 dotenv.config();
 
