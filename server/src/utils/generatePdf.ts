f8113b4ca180175617328ab592609a8f0fd9010e
harshit847 <harshitrai331@gmail.com>
2025-07-23 01:21:24 +0530

    New one

--- server/src/utils/generatePdf.ts
+++ server/src/utils/generatePdf.ts
@@ -1,15 +1,18 @@
+// utils/generatePdf.ts
 import puppeteer from "puppeteer";
 
 export async function generatePDF(htmlContent: string): Promise<Buffer> {
   const browser = await puppeteer.launch({
-    headless: true, 
-    args: ["--no-sandbox", "--disable-setuid-sandbox"], 
-  }); const page = await browser.newPage();
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
+  const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
   const pdf = await page.pdf({ format: "A4" });
+
   await browser.close();
 
-  return Buffer.from(pdf);
+  return Buffer.from(pdf); 
 }
